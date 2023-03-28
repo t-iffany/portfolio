6671b2b5b79b23ab522eb33eedb65c5c2d32c5d4
t-iffany <tiffanyxleong@gmail.com>
2023-03-27 23:29:23 -0700

    Add header and sub-header intro text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { styles } from '../styles';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    // background
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 
+        top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
+          <div className="flex flex-col justify-center items-center mt-5">
+            {/* dot */}
+            <div className="w-5 h-5 rounded-full bg-[#5C9EAD]" />
+            {/* line */}
+            <div className="w-1 sm:h-80 h-40 blue-gradient" />
+          </div>
+
+          <div>
+            <hi className={`${styles.heroHeadText} text-white`}>Hi, I'm &nbsp;
+              <span className="text-[#5C9EAD]">Tiffany</span>
+            </hi>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              I transform concepts into innovative web solutions. 
+            </p>
+          </div>
+
+      </div>
+    </section>
   )
 }
 
