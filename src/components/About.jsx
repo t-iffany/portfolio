a2e7570a37ad07bb3e0d104ecbc0c4d3c1fa9af6
t-iffany <tiffanyxleong@gmail.com>
2023-03-28 00:28:36 -0700

    Add tilting cards to display list of qualities

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,34 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { textVariant, fadeIn } from '../utils/motion';
 import { qualities } from '../constants';
+import Tilt from 'react-parallax-tilt';
 
+// qualities from index.js
 const QualityCard = ({ index, title, icon }) => {
   return (
-    <p>{title}</p>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 
+          min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title}
+            className="w-16 h-16 object-contain"
+          />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
