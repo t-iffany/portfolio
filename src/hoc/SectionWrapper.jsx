9c860f52200bfe3d8a8a6ca5c5364e05dd40d79f
t-iffany <tiffanyxleong@gmail.com>
2023-03-28 00:53:59 -0700

    Give down-scroll icon effect by creating invisible span section

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,3 +1,6 @@
+// SectionWrapper will apply styling and motions to all 
+// sections that we wrap SectionWrapper with
+
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { staggerContainer } from '../utils/motion';
@@ -14,6 +17,11 @@ const SectionWrapper = (Component, idName) =>
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
+        {/* create invisible span section with empty space to create 
+        effect for down-scroll icon */}
+        <span className="hash-span" id={idName}>
+          &nsbp;
+        </span>
         <Component />
       </motion.section>
     );
