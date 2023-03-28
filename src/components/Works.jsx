69599f381dc5f0d12b684e856392609dddad9506
t-iffany <tiffanyxleong@gmail.com>
2023-03-28 15:23:31 -0700

    Add My work:Projects section and headers

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { SectionWrapper } from '../hoc';
+import { motion } from 'framer-motion';
+import { projects } from '../constants';
+import {fadeIn, textVariant } from '../utils/motion';
+import { styles } from '../styles';
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          My work
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Projects.
+        </h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
