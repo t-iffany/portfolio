1b42ec4cc87ef2b2b22f7ff490b70006daeec744
t-iffany <tiffanyxleong@gmail.com>
2023-03-28 21:01:44 -0700

    Add Contact section and create contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import { useState, useRef} from 'react';
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
+import { styles } from '../styles';
+import { motion } from 'framer-motion';
+import { textVariant } from '../utils/motion';
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-export default Contact
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (event) => {
+
+  }
+
+  const handleSubmit = (event) => {
+
+  }
+
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h2 className={styles.sectionHeadText}>Contact.</h2>
+      </motion.div>
+
+    {/* <div className="xl:mt-12 xl:flex-row 
+    flex-col-reverse flex gap-10 overflow-hidden"> */}
+    {/* <div
+        className="mt-12 flex flex-col md:flex-row gap-10 justify-center items-center"
+        style={{ maxWidth: '800px' }}
+      > */}
+    <div className="mt-12 flex justify-center items-center">
+      <motion.div
+        variants={slideIn('left', "tween", 0.2, 1)}
+        className="flex-full bg-[#5C9EAD] p-8 rounded-2xl"
+        style={{ width: "100%", maxWidth: "900px" }}
+      >
+        <p className={`${styles.sectionSubText} bg-[#5C9EAD]`}>
+          Let's
+        </p>
+        <h3 className={`${styles.sectionHeadText} bg-[#5C9EAD]`}>
+          Connect.
+        </h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-5 flex flex-col gap-5 bg-[#5C9EAD]"
+        >
+
+          <label className="flex flex-col bg-[#5C9EAD]">
+            <span className="text-white font-medium mb-4 bg-[#5C9EAD]">
+              Your Name
+            </span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="py-4 px-6 placeholder:text-secondary
+              text-gray rounded-lg outlined-none border-none font-medium"
+              style={{ backgroundColor: "white", width: "100%", maxWidth: "900px" }}
+            />
+          </label>
+
+          <label className="flex flex-col bg-[#5C9EAD]">
+            <span className="text-white font-medium mb-4 bg-[#5C9EAD]">
+              Your Email
+            </span>
+            <input
+              type="text"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="py-4 px-6 placeholder:text-secondary
+              text-gray rounded-lg outlined-none border-none font-medium"
+              style={{ backgroundColor: "white", width: "100%", maxWidth: "900px" }}
+            />
+          </label>
+
+          <label className="flex flex-col bg-[#5C9EAD]">
+            <span className="text-white font-medium mb-4 bg-[#5C9EAD]">
+              Your Message
+            </span>
+            <textarea
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What is your message?"
+              className="py-4 px-6 placeholder:text-secondary
+              text-gray rounded-lg outlined-none border-none font-medium"
+              style={{ backgroundColor: "white", width: "100%", maxWidth: "900px" }}
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white
+            font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {loading ? 'Sending...' : 'Send'}
+          </button>
+
+        </form>
+
+      </motion.div>
+
+    </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
