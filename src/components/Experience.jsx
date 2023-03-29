87e41cc610d4cddfc19f7525e345d32c20f9b308
t-iffany <tiffanyxleong@gmail.com>
2023-03-28 18:16:46 -0700

    Add header and subheader to work/experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+// import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { motion } from 'framer-motion';
+// import 'react-vertical-timeline-component/style.min.css';
+import { styles } from '../styles';
+// import { experiences } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { textVariant } from '../utils/motion';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    // <div>Experience</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My experience</p>
+        <h2 className={styles.sectionHeadText}>Work + Education.</h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Experience
+export default SectionWrapper(Experience, "work");
+
+// export default Experience;
