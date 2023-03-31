7dd08b56c551ac89e2994ac07cdac0248b8b0ef0
t-iffany <tiffanyxleong@gmail.com>
2023-03-30 23:01:44 -0700

    Update header of Tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -10,8 +10,8 @@ const Tech = () => {
     <>
     {/* animate the text when scrolling down */}
     <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>My skills</p>
-        <h2 className={styles.sectionHeadText}>Technologies.</h2>
+        <p className={styles.sectionSubText}>My technical</p>
+        <h2 className={styles.sectionHeadText}>Skills.</h2>
       </motion.div>
 
     <div className="flex flex-row flex-wrap justify-center gap-10">
