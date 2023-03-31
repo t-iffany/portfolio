8c77b559fcb7cefda87ab6e0efe857b7913370c4
t-iffany <tiffanyxleong@gmail.com>
2023-03-30 23:30:59 -0700

    Add tech logos to assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,18 +20,31 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import ruby from "./tech/ruby.svg";
+import sql from "./tech/sql.png";
+import express from "./tech/express.png";
+import rails from "./tech/rails.png";
+import ejs from "./tech/ejs.jpeg";
+import postgresql from "./tech/postgresql.svg";
+import mysql from "./tech/mysql.svg";
+import cypress from "./tech/cypress.svg";
+import jquery from "./tech/jquery.png";
+import storybook from "./tech/storybook.png";
+import jest from "./tech/jest.svg";
+import mocha from ",/tech/mocha.svg";
+import chai from "./tech/chai.jpeg";
+import rspec from "./tech/rspec.png";
+import jira from "./tech/jira.png";
+import slack from "./tech/slack.png";
+import visio from "./tech/visio.png";
+import vite from "./tech/vite.png";
+import spline from "./tech/spline.png";
+import python from "./tech/python.png";
+import flask from "./tech/flask.png";
 
 import cst from "./company/cst.png";
 import vch from "./company/vch.png";
+
 
 export {
   logo,
@@ -55,13 +68,27 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
+  ruby,
+  sql,
+  express,
+  rails,
+  ejs,
+  postgresql,
+  mysql,
+  cypress,
+  jquery,
+  storybook,
+  jest, 
+  mocha,
+  chai,
+  rspec,
+  jira,
+  slack,
+  visio,
+  vite,
+  spline,
+  python,
+  flask,
   cst,
   vch,
 };
