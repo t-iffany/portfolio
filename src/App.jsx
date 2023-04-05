0f0bdb18ca3c1bf48fb278ff9d4a37e42b96f48d
t-iffany <tiffanyxleong@gmail.com>
2023-04-05 01:54:44 -0700

    Remove background image div from navbar + hero

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ function App() {
   return (
       <BrowserRouter>
         <div className="relative z-0 bg-primary">
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <Navbar />
-            <Hero />
-          </div>
+          <Navbar />
+          <Hero />
           <Works />
           <Strengths />
           <Tech />
