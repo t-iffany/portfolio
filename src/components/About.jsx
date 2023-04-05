d2d72504caf83aaa72c3101440e4033acc38fe9a
t-iffany <tiffanyxleong@gmail.com>
2023-04-05 03:24:54 -0700

    Update content of About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,7 +24,8 @@ const About = () => {
           <li>✨ I am comfortable being uncomfortable ✨ </li><br></br>
           <li>Whether it is learning new technologies, making critical decisions in high-pressure environments as a nurse, or solo traveling and living in foreign places around the world.</li> <br></br>  
           <li>I embrace opportunities to be curious, adapt, and grow. </li> <br></br>
-          <li>When I am not learning new tech or coding, these are a few of my other interests! 🐶 ✈️ 🍦 🤸🏻‍♀️ 👩🏻‍⚕️ 🤿 </li>
+          <li>My story: I am a public health nurse transitioned developer. Back in the day, I used to code personal webpages as a hobby. I recall staying up until at least 3am every night, changing my cursor icon, making stars fall from the top of the page, adding looped music, and trying to learn from "view page source". Unfortunately at the time, it was not advertised as a "career" to me. Fast-forward to my last role as an Informatics Specialist, I realized that I was very interested in how the application and system actually works. I started to self-learn coding online, realized that I truly enjoyed it, and decided to quit my job to pursue my passion. I completed an intensive full stack web development bootcamp, and I loved every minute of it. Now here we are!</li> <br></br>
+          <li>A few of my other interests: 🐶 ✈️ 🍦 🤸🏻‍♀️ 👩🏻‍⚕️ 🤿 </li>
         </ul>
       </motion.p>
     </>
