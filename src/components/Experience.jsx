78269d311af9ad687604ea610c4bd1243976fe70
t-iffany <tiffanyxleong@gmail.com>
2023-04-07 13:51:47 -0700

    Remove box borders and adjust styling

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,8 +10,8 @@ import { textVariant } from '../utils/motion';
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
     className="timeline-bg"
-    contentStyle={{ background: "#5C9EAD", color: "#fff" }}
-    contentArrowStyle={{ borderRight: "7px solid #5C9EAD" }}
+    contentStyle={{ background: "transparent", padding: "20px" }}
+    contentArrowStyle={{ borderRight: "none" }}
     date={experience.date}
     iconStyle={{ background: experience.iconBg, borderRadius: "50%" }}
     icon={
@@ -27,19 +27,19 @@ const ExperienceCard = ({ experience }) => (
     }
   >
     <div>
-      <h3 className="text-white text-[24px] font-bold bg-[#5C9EAD]">
+      <h3 className="text-[#5C9EAD] text-[20px] font-bold">
         {experience.title}
       </h3>
-      <p className="text-secondary text-[16px] font-semibold bg-[#5C9EAD]" style={{ margin: 0 }}>
+      <p className="text-[4px] text-secondary text-white" style={{ margin: 0 }}>
         {experience.company_name}
       </p>
     </div>
 
-    <ul className="mt-5 list-disc ml-5 space-y-2 bg-[#5C9EAD]">
+    <ul className="mt-2 list-disc ml-5 space-y-2">
       {experience.points.map((point, index) => (
         <li
           key={`experience-point-${index}`}
-          className="text-white-100 text-[14px] pl-1 tracking-wider bg-[#5C9EAD]"
+          className="text-white-100 text-[14px] pl-1 tracking-wider"
         >
           {point}
         </li>
