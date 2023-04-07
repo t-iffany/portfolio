c52303f42d8e5303464fcd6f1321c2f912fcd5a5
t-iffany <tiffanyxleong@gmail.com>
2023-04-07 02:14:47 -0700

    Change name and title in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
           {/* <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> */}
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Tiffany &nbsp;
-            <span className="sm:block hidden"> | Full Stack Developer</span>
+            <span className="sm:block hidden">• &nbsp;Full Stack Developer</span>
           </p>
         </Link>
 
