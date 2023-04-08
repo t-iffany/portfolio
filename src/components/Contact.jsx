be1e782a0a19a225a4f13ecb16cfb0cd5bacac4f
t-iffany <tiffanyxleong@gmail.com>
2023-04-08 12:37:06 -0700

    Add left column of icons and details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,13 @@
-import { useState, useRef} from 'react';
+import { useState, useRef } from 'react';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 import { styles } from '../styles';
 import { motion } from 'framer-motion';
 import { textVariant } from '../utils/motion';
 import emailjs from '@emailjs/browser';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 const EMAILJS_SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const EMAILJS_TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
@@ -23,15 +26,15 @@ const Contact = () => {
   const handleChange = (event) => {
     const { name, value } = event.target;
 
-    setForm({ ...form, [name]: value })
-  }
+    setForm({ ...form, [name]: value });
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setLoading(true);
 
     emailjs.send(
-      EMAILJS_SERVICE_ID, 
+      EMAILJS_SERVICE_ID,
       EMAILJS_TEMPLATE_ID,
       {
         from_name: form.name,
@@ -41,24 +44,24 @@ const Contact = () => {
         message: form.message,
       },
       EMAILJS_API_KEY,
-    ) 
-    .then (() => {
-      setLoading(false);
-      alert('Thank you! I will get back to you as soon as possible.');
+    )
+      .then(() => {
+        setLoading(false);
+        alert('Thank you! I will get back to you as soon as possible.');
 
-      setForm({
-        email: "",
-        name: "",
-        message: "",
-      }, (error) => {
-        setLoading(false)
+        setForm({
+          email: "",
+          name: "",
+          message: "",
+        }, (error) => {
+          setLoading(false);
 
-        console.log(error);
+          console.log(error);
 
-        alert('Email could not be sent. Please try again.')
-      })
-    })
-  }
+          alert('Email could not be sent. Please try again.');
+        });
+      });
+  };
 
   return (
     <>
@@ -68,94 +71,113 @@ const Contact = () => {
         &nbsp;
       </motion.div>
 
-    {/* <div className="xl:mt-12 xl:flex-row 
+      {/* <div className="xl:mt-12 xl:flex-row 
     flex-col-reverse flex gap-10 overflow-hidden"> */}
-    {/* <div
+      {/* <div
         className="mt-12 flex flex-col md:flex-row gap-10 justify-center items-center"
         style={{ maxWidth: '800px' }}
       > */}
-    <div className="flex justify-center items-center -mt-32"
-      style={{ display: "flex", justifyContent: "flex-start" }}
-    >
-      <motion.div
-        variants={slideIn('left', "tween", 0.2, 1)}
-        className="flex-full bg-[#5C9EAD] p-8 rounded-2xl"
-        style={{ width: "100%", maxWidth: "900px", scale: "0.6" }}
-      >
-        {/* <p className={`${styles.sectionSubText} bg-[#5C9EAD]`}>
+
+
+      {/* <div className="flex justify-center items-center -mt-32"
+        style={{ display: "flex", justifyContent: "flex-start" }}
+      > */}
+
+        <motion.div
+          variants={slideIn('left', "tween", 0.2, 1)}
+          // className="flex-full bg-[#5C9EAD] p-8 rounded-2xl"
+          className="flex flex-col md:flex-row justify-center items-center -mt-32"
+          style={{ display: "flex", justifyContent: "flex-start", width: "100%", maxWidth: "900px", backgroundColor: "transparent" }}
+        >
+          {/* <p className={`${styles.sectionSubText} bg-[#5C9EAD]`}>
           Let's
         </p>
         <h3 className={`${styles.sectionHeadText} bg-[#5C9EAD]`}>
           Connect.
         </h3> */}
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className="flex flex-col gap-5 bg-[#5C9EAD]"
-        >
+        <div className="contact-icons flex flex-row md:flex-col items-center justify-start md:justify-center mb-8 md:mb-0">
+          <FontAwesomeIcon icon={faEnvelope} size="lg" color="#5C9EAD" />
+          <span className="mr-6 md:mr-0 md:text-center">tiffanyxleong@gmail.com</span>
+          <FontAwesomeIcon icon={faMapMarkerAlt} size="lg" color="#5C9EAD" />
+          Vancouver, B.C. <br/>
+          Open to relocating
+          <a href="https://www.linkedin.com/in/tiffanyleong/" target="_blank" className="mr-10 mb-3">
+            <FontAwesomeIcon icon={faLinkedin} className="fa-lg w-6 h-6" color="#5C9EAD" /> <br/>
+            tiffanyleong
+          </a>
+        </div>
 
-          <label className="flex flex-col bg-[#5C9EAD]">
-            <span className="text-white font-medium mb-4 bg-[#5C9EAD]">
-              Your Name
-            </span>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your name?"
-              className="py-4 px-6 placeholder:text-secondary
+          {/* <div className="form"> */}
+            <form
+              ref={formRef}
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-5 bg-[#5C9EAD] p-8 rounded-2xl flex-full"
+              style={{ width: "100%", maxWidth: "900px", scale: "0.6"}}
+            >
+
+              <label className="flex flex-col bg-[#5C9EAD]">
+                <span className="text-white font-medium mb-4 bg-[#5C9EAD]">
+                  Your Name
+                </span>
+                <input
+                  type="text"
+                  name="name"
+                  value={form.name}
+                  onChange={handleChange}
+                  placeholder="What's your name?"
+                  className="py-4 px-6 placeholder:text-secondary
               text-black rounded-lg outlined-none border-none font-medium"
-              style={{ backgroundColor: "white", width: "100%", maxWidth: "900px" }}
-            />
-          </label>
+                  style={{ backgroundColor: "white", width: "100%", maxWidth: "900px" }}
+                />
+              </label>
 
-          <label className="flex flex-col bg-[#5C9EAD]">
-            <span className="text-white font-medium mb-4 bg-[#5C9EAD]">
-              Your Email
-            </span>
-            <input
-              type="text"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your email?"
-              className="py-4 px-6 placeholder:text-secondary
+              <label className="flex flex-col bg-[#5C9EAD]">
+                <span className="text-white font-medium mb-4 bg-[#5C9EAD]">
+                  Your Email
+                </span>
+                <input
+                  type="text"
+                  name="email"
+                  value={form.email}
+                  onChange={handleChange}
+                  placeholder="What's your email?"
+                  className="py-4 px-6 placeholder:text-secondary
               text-black rounded-lg outlined-none border-none font-medium"
-              style={{ backgroundColor: "white", width: "100%", maxWidth: "900px" }}
-            />
-          </label>
+                  style={{ backgroundColor: "white", width: "100%", maxWidth: "900px" }}
+                />
+              </label>
 
-          <label className="flex flex-col bg-[#5C9EAD]">
-            <span className="text-white font-medium mb-4 bg-[#5C9EAD]">
-              Your Message
-            </span>
-            <textarea
-              rows="7"
-              name="message"
-              value={form.message}
-              onChange={handleChange}
-              placeholder="What is your message?"
-              className="py-4 px-6 placeholder:text-secondary
+              <label className="flex flex-col bg-[#5C9EAD]">
+                <span className="text-white font-medium mb-4 bg-[#5C9EAD]">
+                  Your Message
+                </span>
+                <textarea
+                  rows="7"
+                  name="message"
+                  value={form.message}
+                  onChange={handleChange}
+                  placeholder="What's your message?"
+                  className="py-4 px-6 placeholder:text-secondary
               text-black rounded-lg outlined-none border-none font-medium"
-              style={{ backgroundColor: "white", width: "100%", maxWidth: "900px" }}
-            />
-          </label>
+                  style={{ backgroundColor: "white", width: "100%", maxWidth: "900px" }}
+                />
+              </label>
 
-          <button
-            type="submit"
-            className="bg-tertiary py-3 px-8 outline-none w-fit text-white
+              <button
+                type="submit"
+                className="bg-tertiary py-3 px-8 outline-none w-fit text-white
             font-bold shadow-md shadow-primary rounded-xl"
-          >
-            {loading ? 'Sending...' : 'Send'}
-          </button>
+              >
+                {loading ? 'Sending...' : 'Send'}
+              </button>
 
-        </form>
+            </form>
+          {/* </div> */}
 
-      </motion.div>
+        </motion.div>
 
-    </div>
+      {/* </div> */}
     </>
   );
 };
