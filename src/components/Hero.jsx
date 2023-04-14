8a5cb7cbe8294b06f7483c03ef21dda607fd9e20
t-iffany <tiffanyxleong@gmail.com>
2023-04-14 14:41:55 -0700

    Increase y of viewbox

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -121,7 +121,7 @@ return (
                         xmlns="http://www.w3.org/2000/svg"
                         width="20"
                         height="20"
-                        viewBox="0 0 24 24"
+                        viewBox="0 0 24 30"
                         fill="none"
                         stroke="#59A7C1"
                         strokeWidth="4"
