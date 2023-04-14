87c927cf3bac74675ff7a491d01ba19b63dcf976
t-iffany <tiffanyxleong@gmail.com>
2023-04-14 00:21:53 -0700

    Change secondary color to blue and tertiary color to white

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,8 @@ module.exports = {
     extend: {
       colors: {
         primary: "#282928",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
+        secondary: "#59A7C1",
+        tertiary: "#f3f3f3",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
